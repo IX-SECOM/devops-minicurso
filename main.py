057622739d7965e94b97a1410dd9ac81034439ca
José Nunes <josenunesra@gmail.com>
2020-11-06 15:58:05 -0300

    add github actions

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, render_template, request, redirect, url_for
 
 from form import IMCForm
@@ -41,4 +43,5 @@ def all_exception_handler(error_):
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
